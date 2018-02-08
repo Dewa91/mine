d12061c2c361076aa08ded31aac0804b43488b42
dewaantara <32819460+dewaantara@users.noreply.github.com>
2018-02-08 03:26:19 -0800

    Update cnts.sh

--- cnts.sh
+++ cnts.sh
@@ -11,20 +11,5 @@ yum install -y epel-release
 yum -y install centos-release-scl cmake3 hwloc-devel libmicro httpd-devel openssl-devel git screen nano devtoolset-4-gcc*  
 sleep 1
 sudo scl enable devtoolset-4 bash
-git clone https://github.com/fireice-uk/xmr-stak-cpu.git
-cd xmr-stak-cpu
-cmake3 -DMICROHTTPD_ENABLE=OFF
-make install
-cd bin/
-rm -rf config.txt
-wget https://raw.githubusercontent.com/dewaantara/miner/master/config.txt
-chmod +x xmr-stak-cpu
-sysctl -w vm.nr_hugepages=128
-
+echo "lanjut..."
 sleep 1
-cd ~/xmr-stak-cpu/bin
-screen -dmS mining ./xmr-stak-cpu
-echo "script mining telah dijalankan"
-echo "untuk menutup ctrl+a+d agar tetap jalan miningnya ^_^"
-echo ""
-echo "siap grak terima gaji ^_^"
